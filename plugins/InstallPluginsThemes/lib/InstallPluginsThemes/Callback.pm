13340f5e21142d190019b4ca2c33648931996cbf
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2014-02-22 15:19:16 +0900

    Modify links to Plugins and Themes management screen. #3

--- plugins/InstallPluginsThemes/lib/InstallPluginsThemes/Callback.pm
+++ plugins/InstallPluginsThemes/lib/InstallPluginsThemes/Callback.pm
@@ -5,25 +5,44 @@ use warnings;
 # Change 'Find Themes' link.
 sub tmpl_src_list_theme {
     my ( $cb, $app, $tmpl_ref ) = @_;
-    _replace_anchor_tag( $app, $tmpl_ref,
-        '<__trans phrase="_THEME_DIRECTORY_URL">' );
+    _replace_anchor_tag(
+        {   app        => $app,
+            tmpl_ref   => $tmpl_ref,
+            pre_url    => '<__trans phrase="_THEME_DIRECTORY_URL">',
+            filter_key => 'themes_only',
+        }
+    );
 }
 
 # Change 'Find Plugins' link.
 sub tmpl_src_cfg_plugin {
     my ( $cb, $app, $tmpl_ref ) = @_;
-    _replace_anchor_tag( $app, $tmpl_ref,
-        '<__trans phrase="_PLUGIN_DIRECTORY_URL">' );
+    _replace_anchor_tag(
+        {   app        => $app,
+            tmpl_ref   => $tmpl_ref,
+            pre_url    => '<__trans phrase="_PLUGIN_DIRECTORY_URL">',
+            filter_key => 'plugins_only',
+        }
+    );
 }
 
 sub _replace_anchor_tag {
-    my ( $app, $tmpl_ref, $pre_url ) = @_;
+    my $args = shift;
+
+    my $app        = $args->{app};
+    my $tmpl_ref   = $args->{tmpl_ref};
+    my $pre_url    = $args->{pre_url};
+    my $filter_key = $args->{filter_key};
 
     # Replace URL.
     $pre_url = quotemeta $pre_url;
     my $post_url = $app->uri(
         mode => 'list',
-        args => { _type => 'plugin_theme', blog_id => 0 }
+        args => {
+            _type      => 'plugin_theme',
+            blog_id    => 0,
+            filter_key => $filter_key,
+        },
     );
     $$tmpl_ref =~ s/$pre_url/$post_url/;
 
